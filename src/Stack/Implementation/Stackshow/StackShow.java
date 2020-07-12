aa8e93c377d784b582d9b1c2693b33138e48016c
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-12 22:47:08 +0530

    StackShow() method starts.

--- src/Stack/Implementation/Stackshow/StackShow.java
+++ src/Stack/Implementation/Stackshow/StackShow.java
@@ -1,4 +1,19 @@
 package Stack.Implementation.Stackshow;
 
+/**
+ * This is the class for showing the Stack implementation.
+ * There are three operations in the stack:
+ * 1. push - to put item on top of stack.
+ * 2. pop - to remove item from the top of stack.
+ * 3. peek - to access the element at topmost index of stack.
+ */
 public class StackShow {
+    /**
+     * instance variables.
+     * intArray[] to store the data items.
+     * nElems to store the number of data items.
+     */
+    private int[] intArray;
+    private int nElems;
+
 }
