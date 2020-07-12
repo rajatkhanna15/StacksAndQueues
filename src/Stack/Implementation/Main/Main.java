ee6188c47157c182e6e50f19f672f13fbcfc4a0d
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-12 23:15:24 +0530

    display() method starts.

--- src/Stack/Implementation/Main/Main.java
+++ src/Stack/Implementation/Main/Main.java
@@ -1,5 +1,7 @@
 package Stack.Implementation.Main;
 
+import Stack.Implementation.Stackshow.StackShow;
+
 import java.util.*;
 
 /**
@@ -7,4 +9,16 @@ import java.util.*;
  */
 public class Main {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StackShow st = new StackShow(100);
+        System.out.println("How many elements you want to enter in the stack? ");
+        int n =scanner.nextInt();
+        System.out.println("Enter the elements :");
+        for (int i = 0; i < n; i++) {
+            int x = scanner.nextInt();
+            st.push(x);
+        }
+
+    }
 }
--- src/Stack/Implementation/Stackshow/StackShow.java
+++ src/Stack/Implementation/Stackshow/StackShow.java
@@ -71,4 +71,5 @@ public class StackShow {
         return top == nElems-1;
     }
 
+
 }
