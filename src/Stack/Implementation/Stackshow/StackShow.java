2ff27141d4ac5c65337dd7d1fe2bbdbb79562ddd
Rajat Khanna <rajat.khanna_cs18@gla.ac.in>
2020-07-12 23:16:22 +0530

    display() method ends.

--- src/Stack/Implementation/Stackshow/StackShow.java
+++ src/Stack/Implementation/Stackshow/StackShow.java
@@ -71,5 +71,14 @@ public class StackShow {
         return top == nElems-1;
     }
 
+    /**
+     * display() elements to display the elements.
+     */
+    public void display()
+    {
+        for (int i = 0; i < nElems; i++) {
+            System.out.print(intArray[i] + " ");
+        }
+    }
 
 }
